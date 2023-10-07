1cdbdc1aec197834325b86046f2506ea43881333
Dhika <udinjogres13@gmail.com>
2023-10-07 19:05:21 +0700

    pull request testing

--- test.js
+++ test.js
@@ -1,2 +1,24 @@
 let test3 ="Testing git"
 console.log(test3)
+
+let testing = "testing variable let"
+
+function testFunc() {
+    let testing = "block scope"
+    return testing
+}
+console.log(testing)
+console.log(testFunc())
+
+const asd = [111]
+
+asd.push(1233)
+console.log(asd)
+
+console.log(testing.split(" ").join(""))
+
+const numb1 = 12
+const numb2 = 3
+
+const boolean = null == undefined
+console.log(boolean)
